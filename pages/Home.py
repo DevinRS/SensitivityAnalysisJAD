3c8cc48d7823dcc972938283a81461ecfd9d07be
Devin Setiawan <90437494+DevinRS@users.noreply.github.com>
2023-09-12 14:49:42 -0500

    Add files via upload

---
+++ pages/Home.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.sidebar.success('Select Mission Above 👆')
+st.title('Jayhawk Aero Design')
+st.header('AIAA 2023-2024')
+st.write('The objective for this year is to design, build, and test an airplane to demonstrate Urban Air Mobility (UAM) missions. Flight missions will include delivery of the airplane, medical transport, and urban taxi.')
+st.subheader('👉[Rules](https://www.aiaa.org/docs/default-source/uploadedfiles/aiaadbf/resources/dbf-rules-2024-draft-4.pdf?sfvrsn=8d6c3a8a_0)')
