806af40808caabbdc1281f456d834013fa3a8d90
Devin Setiawan <90437494+DevinRS@users.noreply.github.com>
2023-09-12 14:50:54 -0500

    New Home.py

--- Home.py
+++ Home.py
@@ -1,36 +1,7 @@
 import streamlit as st
-from PIL import Image
-from streamlit_drawable_canvas import st_canvas
-
 
 st.sidebar.success('Select Mission Above 👆')
-# Specify canvas parameters in application
-drawing_mode = st.sidebar.selectbox(
-    "Drawing tool:",
-    ("freedraw", "line", "rect", "circle", "transform", "polygon", "point"),
-)
-stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
-if drawing_mode == "point":
-    point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
-stroke_color = st.sidebar.color_picker("Stroke color hex: ")
-bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
-bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
-realtime_update = st.sidebar.checkbox("Update in realtime", True)
-
-st.title('Jayhawk Aero Design 🛩️')
-st.subheader('Here\'s a Canvas 🖌️')
-# Create a canvas component
-canvas_result = st_canvas(
-    fill_color="rgba(255, 165, 0, 0.3)",  # Fixed fill color with some opacity
-    stroke_width=stroke_width,
-    stroke_color=stroke_color,
-    background_color=bg_color,
-    background_image=Image.open(bg_image) if bg_image else None,
-    update_streamlit=realtime_update,
-    height=400,
-    drawing_mode=drawing_mode,
-    point_display_radius=point_display_radius if drawing_mode == "point" else 0,
-    display_toolbar=st.sidebar.checkbox("Display toolbar", True),
-    key="full_app",
-    width=800,
-)
+st.title('Jayhawk Aero Design')
+st.header('AIAA 2023-2024')
+st.write('The objective for this year is to design, build, and test an airplane to demonstrate Urban Air Mobility (UAM) missions. Flight missions will include delivery of the airplane, medical transport, and urban taxi.')
+st.subheader('👉[Rules](https://www.aiaa.org/docs/default-source/uploadedfiles/aiaadbf/resources/dbf-rules-2024-draft-4.pdf?sfvrsn=8d6c3a8a_0)')
